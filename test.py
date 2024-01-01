5b583f34db9b941c5060f1369f2326c484f344ac
Blueishfiend692 <aditymakkar000@gmail.com>
2024-01-01 13:21:38 -0500

    fixed image

--- test.py
+++ test.py
@@ -28,8 +28,10 @@ def update(t):
           else:
             grid[i,j] = grid[i,j] + k*(grid[i+1,j] + grid[i-1,j] + grid[i,j+1] + grid[i,j-1] - 4*grid[i,j])/cell_size**2
     im.set_array(grid)
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
     ax.set_title(f"t = {t*dt:.2f}")
-    return im, ax.title,
+    return im, ax.title, ax.xaxis.label, ax.yaxis.label
 
 bbox = [0, 0, 100000, 100000]
 cell_size = 5000
@@ -38,14 +40,14 @@ cols = int((bbox[2] - bbox[0]) / cell_size)
 
 fig, ax = plt.subplots()
 grid = np.zeros((rows, cols), dtype=np.int32)
+cbar = fig.colorbar(ax.imshow(grid, cmap='hot', interpolation='nearest'))
 
 
 for i in range(rows):
     for j in range(cols):
-        grid[i, j] = math.cos(i)*100
+        grid[i, j] = math.cos(i)*100 + math.sin(j)*100
 
-
-time_range = 220
+time_range = 200
 dt = 1
 k = 1
 time_steps = np.arange(0, time_range, dt)
@@ -53,4 +55,4 @@ time_steps = np.arange(0, time_range, dt)
 im = ax.imshow(grid, animated=True, cmap='hot', interpolation='nearest')
 ani = animation.FuncAnimation(fig, update, frames=time_steps, interval=50, blit=True)
 
-ani.save('heat_equation_test.gif', writer='imagemagick')
+ani.save('heat_equation_2d.gif', writer='imagemagick')
