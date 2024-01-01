74ef8fd9918d36f37e58a058058fc0208f48f715
Blueishfiend692 <aditymakkar000@gmail.com>
2024-01-01 12:08:42 -0500

    2d heat diffusion with apporximate neumann boundary conditions

---
+++ test.py
@@ -0,0 +1,52 @@
+import numpy as np
+import pandas as pd
+import math
+import matplotlib.pyplot as plt
+
+
+bbox = [0, 0, 100000, 100000]
+cell_size = 5000
+rows = int((bbox[3] - bbox[1]) / cell_size)
+cols = int((bbox[2] - bbox[0]) / cell_size)
+
+grid = np.zeros((rows, cols), dtype=np.int32)
+
+
+for i in range(rows):
+    for j in range(cols):
+        grid[i, j] = math.cos(i)*100 + math.sin(j)*100
+import numpy as np
+
+time_range = 1000
+dt = 1
+k = 1
+time_steps = np.arange(0, time_range, dt)
+
+for t in time_steps:
+    for i in range(rows):
+        for j in range(cols):
+            if i == 0 or i == rows-1 or j == 0 or j == cols-1:
+              if i == 0 and j == 0:
+                grid[i,j] = grid[i,j] + k*(grid[i+1,j] + grid[i,j+1] - 2*grid[i,j])/cell_size**2
+              elif i == 0 and j == cols-1:
+                grid[i,j] = grid[i,j] + k*(grid[i+1,j] + grid[i,j-1] - 2*grid[i,j])/cell_size**2
+              elif i == rows-1 and j == 0:
+                grid[i,j] = grid[i,j] + k*(grid[i-1,j] + grid[i,j+1] - 2*grid[i,j])/cell_size**2
+              elif i == rows-1 and j == cols-1:
+                grid[i,j] = grid[i,j] + k*(grid[i-1,j] + grid[i,j-1] - 2*grid[i,j])/cell_size**2
+              elif i == 0:
+                grid[i,j] = grid[i,j] + k*(grid[i+1,j] + grid[i,j+1] + grid[i,j-1] - 3*grid[i,j])/cell_size**2
+              elif i == rows-1:
+                grid[i,j] = grid[i,j] + k*(grid[i-1,j] + grid[i,j+1] + grid[i,j-1] - 3*grid[i,j])/cell_size**2
+              elif j == 0:
+                grid[i,j] = grid[i,j] + k*(grid[i+1,j] + grid[i-1,j] + grid[i,j+1] - 3*grid[i,j])/cell_size**2
+              elif j == cols-1:
+                grid[i,j] = grid[i,j] + k*(grid[i+1,j] + grid[i-1,j] + grid[i,j-1] - 3*grid[i,j])/cell_size**2
+            else:
+              grid[i,j] = grid[i,j] + k*(grid[i+1,j] + grid[i-1,j] + grid[i,j+1] + grid[i,j-1] - 4*grid[i,j])/cell_size**2
+
+    plt.imshow(grid, cmap='hot', interpolation='nearest')
+    plt.colorbar()
+    plt.pause(0.01)
+    plt.clf()
+
