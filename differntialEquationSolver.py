3ae89ac68c7cc853c60c14e264676242f6c9b3e1
Blueishfiend692 <aditymakkar000@gmail.com>
2024-01-28 10:56:20 -0500

    differntial solver

--- differntialEquationSolver.py
+++ differntialEquationSolver.py
@@ -1,3 +1,6 @@
+
+# differentialEquationSolver for a second order differential equation
+
 import numpy as np
 import math
 import matplotlib.pyplot as plt
@@ -6,27 +9,31 @@ import matplotlib.pyplot as plt
 def grad_x(coords):
     x = coords[0]
     y = coords[1]
-    return y
+    # return the first equation
+    return f1
 def grad_y(coords):
     x = coords[0]
     y = coords[1]
-    return -1/2*x + 5/2*y
+    # return the second equation
+    return f2
 
 
 size = 1000
 lin = np.linspace(-1*size,size,10)
 x,y = np.meshgrid(lin,lin)
-pf_px = y
-pf_py = -1/2*x + 5/2*y
-print(pf_px)
-print(pf_py)
-d = 0.1
-intial_point = [[3,4]]
+pf_px = f1 # also first equation
+pf_py = f2 # also second equation
+# print(pf_px)
+# print(pf_py)
+d = 0.1 # step size
+intial_point = [[3,4]] # initial condition
 
 plt.ion()
 
 for i in range(0,100):
   plt.quiver(x,y,pf_px, pf_py)
+  # use for loop for trace remove it if you want no trace and
+  # just make intial_point a 1x2 array and then directly update that instead 
   for point in intial_point:
     plt.plot(point[0],point[1],'ro')
   point = intial_point[len(intial_point)-1]
